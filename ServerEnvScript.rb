3a0a752274ea03b5d8291bab828c9b7a2d8aeca6
govind45 <govindarajulu920@gmail.com>
2016-04-01 15:48:46 +0530

    Update ServerEnvScript.rb

--- ServerEnvScript.rb
+++ ServerEnvScript.rb
@@ -2,12 +2,10 @@
 require 'fileutils'
 require 'yaml'
 yml_file = YAML.load_file('config.yml')
-folder  = "/home/ec2-user/.jenkins/jobs/server/workspace/"
 count=0
-Dir.chdir folder
-result = File.exist?("TEST-com.netenrich.BasicTests.txt")
+result = File.exist?("TEST-com.netenrich.ServerEnvTestCases.txt")
 if result==true
-        file_handler = File.open("TEST-com.netenrich.BasicTests.txt")
+        file_handler = File.open("TEST-com.netenrich.ServerEnvTestCases.txt")
         for line in file_handler do
                 count=count+1
                 if count==2
@@ -21,7 +19,7 @@ else
 end
 puts total_tests
 puts failed_tests
-FileUtils.rm_rf("TEST-com.netenrich.BasicTests.txt")
+FileUtils.rm_rf("TEST-com.netenrich.ServerEnvTestCases.txt")
 if failed_tests == 0
         puts "Code will Deploy to Test Environment"
 else
